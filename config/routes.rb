683bed837145afae88d205ed658c201f4148ddf2
yonmi08 <sorairo.benimairoi@gmail.com>
2020-03-23 14:16:44 +0900

    [WIP]view
    view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'pinks/index'
+  root 'pinks#index'
+
+
+end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
